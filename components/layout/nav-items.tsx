903cfce884dea32baaeaa1408f16b931385d6d14
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-03-27 23:23:00 +0100

    feat: install lucide
    add lucide icons to nav-items. Also fixed the error caused as typescript only processes jsx in tsx file

--- components/layout/nav-items.tsx
+++ components/layout/nav-items.tsx
@@ -1,29 +1,30 @@
 import { NavItems } from "@/types/nav";
+import { LayoutGrid, FolderKanban, Activity, Settings } from "lucide-react";
 
 export const navItems: NavItems[] = [
   {
     label: "Overview",
     href: "/",
-    icon: null,
-  },
-  {
-    label: "Activity",
-    href: "/activity",
-    badge: 6,
-    icon: null,
+    icon: <LayoutGrid size={14} />,
   },
   {
     label: "Projects",
     badge: 12,
     href: "/projects",
-    icon: null,
+    icon: <FolderKanban size={14} />,
   },
+  {
+      label: "Activity",
+      href: "/activity",
+      badge: 6,
+      icon: <Activity size={14} />,
+    },
 ];
 
 export const accountItems : NavItems[] = [
     {
         label: "Settings",
         href: '/settings',
-        icon: null
+        icon: <Settings size={14} />
     }
 ]
