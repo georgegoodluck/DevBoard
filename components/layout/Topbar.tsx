c1f8cdf1c086f8d5c389fea333b14d6f93183614
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-03-28 23:05:49 +0100

    feat: create TopbarSearch

--- components/layout/Topbar.tsx
+++ components/layout/Topbar.tsx
@@ -1,10 +1,12 @@
 import TopbarBreadcrumb from "./TopbarBreadcrumb";
+import TopbarSearch from "./TopbarSearch";
 
 export default function Topbar() {
   return (
-    <header className="p-2 bg-[var(--bg1)]" 
+    <header className="flex items-center gap-3 p-2 bg-[var(--bg1)]" 
     style={{ height: "var(--topbar-height)" }}>
       <TopbarBreadcrumb />
+      <TopbarSearch />
     </header>
   );
 }
