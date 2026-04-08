a4839b7ca7b010425cb746fd477b230d53c401e0
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-08 22:43:03 +0100

    feat: create deadline structure

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -3,6 +3,7 @@ import ActiveProjects from "@/components/layout/overview/ActiveProjects";
 import RecentTasks from "@/components/layout/overview/RecentTasks";
 import SprintVelocity from "@/components/layout/overview/SprintVelocity";
 import TeamPanel from "@/components/layout/overview/TeamPanel";
+import Deadline from "@/components/layout/overview/Deadline"
 
 export default function OverviewPage() {
   return (
@@ -21,6 +22,7 @@ export default function OverviewPage() {
         <div className="flex flex-col w-80">
           <SprintVelocity />
           <TeamPanel />
+          <Deadline />
         </div>
       </div>
     </div>
--- components/layout/overview/Deadline.tsx
+++ components/layout/overview/Deadline.tsx
@@ -1,7 +1,12 @@
-type deadline = {
-    
-}
-
 export default function Deadline() {
-
+  return (
+    <div className="bg-(--bg1) border border-(--border) rounded-(--radius) overflow-hidden">
+      <div className="flex items-center px-3.5 py-3">
+        <div className="flex items-center font-mono font-semibold text-[13px] uppercase tracking-[0.08em] text-(--text2) gap-2">
+          <span className="w-1.5 h-1.5 bg-(--danger) rounded-full"></span>
+          Deadlines
+        </div>
+      </div>
+    </div>
+  );
 }
