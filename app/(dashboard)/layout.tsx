ebaa7599f894ad7770ae02640ba76e620e06a0d6
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-09 11:56:18 +0100

    feat: add responsive design to sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,12 +7,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <div className="flex h-screen overflow-hidden bg-(--bg)">
+      {/* Sidebar - hidden on mobile, visible lg */}
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
       <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
         <Topbar />
-        <main className="flex-1 overflow-y-auto p-5 w-full">
-          {children}</main>
+        <main className="flex-1 overflow-y-auto p-5 w-full">{children}</main>
       </div>
     </div>
   );
