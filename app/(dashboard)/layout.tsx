0681b6d6094cd527739641a68197ea4f8d9f322a
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-09 14:33:19 +0100

    fix: fix redundant class and invalid spacing value

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -15,15 +15,15 @@ export default function DashboardLayout({
       </div>
       <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
         <Topbar />
-        <main className="flex-1 overflow-y-auto p-5 lg:p-5 pb-18 lg:pb-5">{children}</main>
+        <main className="flex-1 overflow-y-auto p-5 pb-20 lg:pb-5">
+          {children}
+        </main>{" "}
       </div>
-
 
       {/* BottomNav - visible on mobile display */}
       <div className="lg:hidden">
         <BottomNav />
       </div>
-      
     </div>
   );
 }
