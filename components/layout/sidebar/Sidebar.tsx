b029b6b80262e8ae20430ebae2b41a728d74e78e
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-09 16:30:14 +0100

    refactor: remove redundant styling in Sidebar

--- components/layout/sidebar/Sidebar.tsx
+++ components/layout/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import SidebarUser from "./SidebarUser";
 export default function Sidebar() {
   return (
     <aside
-      className="flex flex-col shrink-0 overflow-hidden border-r border-[var(--border)] bg-[var(--bg1)]"
+      className="flex flex-col shrink-0 overflow-hidden border-r border-(--border) bg-(--bg1)"
       style={{ width: "var(--sidebar-width)", height: "100vh" }}
     >
       <SidebarLogo />
