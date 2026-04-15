d0609f68b0f6dad9ba431e60cc7760da68be5d92
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-15 13:39:44 +0100

    feat: add imports

---
+++ components/projects/ProjectCard.tsx
@@ -0,0 +1,12 @@
+import Badge from "@/components/ui/Badge";
+import Avatar from "@/components/ui/Avatar";
+import BadgeVariant from "@/components/ui/Badge";
+import ProgressBar from "@/components/ui/ProgressBar";
+
+export default function ProjectCard() {
+  return (
+    <div>
+      <h1>Project Card</h1>
+    </div>
+  );
+}
