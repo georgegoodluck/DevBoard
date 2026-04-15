a2ac2433e71db369b58333e17cba1797f2342e29
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-15 15:46:52 +0100

    feat: add imports to ProjectGrid

--- components/projects/ProjectGrid.tsx
+++ components/projects/ProjectGrid.tsx
@@ -1,7 +1,11 @@
+import ProjectCard, { Projects } from "./ProjectCard";
+import EmptyState from "../ui/EmptyState";
+import { FolderKanban } from "lucide-react";
+
 export default function ProjectGrid() {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
