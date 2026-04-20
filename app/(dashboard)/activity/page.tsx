40682f5fb1034af5ac5ab1cd07a4e2e7b477c9fe
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-20 02:40:44 +0100

    feat: create ContributionsPanel

--- app/(dashboard)/activity/page.tsx
+++ app/(dashboard)/activity/page.tsx
@@ -1,9 +1,11 @@
 import ActivityFeed from "@/components/activity/ActivityFeed";
+import ContributionsPanel from "@/components/activity/ContributionsPanel";
 
 export default function ActivityPage() {
   return (
     <div>
       <ActivityFeed />
+      <ContributionsPanel />
     </div>
   );
 }
