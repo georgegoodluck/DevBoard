3c255c3cdba56753d1ebd6b17c5d2dfe047aa57e
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-24 02:34:25 +0100

    feat: create pipeline data

--- components/activity/CicdStatus.tsx
+++ components/activity/CicdStatus.tsx
@@ -1,36 +1,14 @@
-const status = [
-  {
-    name: "TickrPay",
-    status: "Passing",
-    statusColor: "green",
-  },
-
-  {
-    // gradient:
-    name: "Pulse",
-    status: "Failed",
-    statusCOlor: "danger",
-  },
-
-  {
-    // gradient:
-    name: "Fin·Snap",
-    status: "Passing",
-    statusColor: "green",
-  },
-
-  {
-    // gradient:
-    name: "SubTrack",
-    status: "Deploying",
-    statusColor: "amber",
-  },
+const pipelines = [
+  { name: "TickrPay", status: "Passing" as const },
+  { name: "Pulse", status: "Failed" as const },
+  { name: "fin·snap", status: "Passing" as const },
+  { name: "SubTrack", status: "Deploying" as const },
 ];
 
 export default function CicdStatus() {
-    return (
-        <>
-        <h1>CI / CD Status</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>CI / CD Status</h1>
+    </>
+  );
 }
