1367000fa6bf0774cb834f2faa49f5ded5b62320
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-26 19:16:46 +0100

    feat: create NotificationSettings

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,9 +1,11 @@
+import NotificationSettings from "@/components/settings/NotificationSettings";
 import ProfileSettings from "@/components/settings/ProfileSettings";
 
 export default function SettingsPage() {
   return (
     <div>
       <ProfileSettings />
+      <NotificationSettings />
     </div>
   );
 }
---
+++ components/settings/NotificationSettings.tsx
@@ -0,0 +1,7 @@
+export default function NotificationSettings() {
+  return (
+    <div>
+      <h1>Notification</h1>
+    </div>
+  );
+}
