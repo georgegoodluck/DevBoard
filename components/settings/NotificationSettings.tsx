f933ee270af1c3d8b2daeeb859eb32ca1421ab1e
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-26 19:37:22 +0100

    feat: create Setting type
    - Add Setting type with label, description, key, and default fields

--- components/settings/NotificationSettings.tsx
+++ components/settings/NotificationSettings.tsx
@@ -1,5 +1,12 @@
 import CardHeader from "@/components/ui/CardHeader";
 
+type Setting = {
+  label: string;
+  value: string;
+  key: string;
+  default: boolean;
+};
+
 export default function NotificationSettings() {
   return (
     <div className="border border-(--border) rounded-(--radius) bg-(--bg1 overflow-hidden">
