88fa66f1f85a427a82c152aefe8716948827a7f7
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-27 11:54:00 +0100

    feat: create DangerZoneSettings component

---
+++ components/settings/DangerZoneSettings.tsx
@@ -0,0 +1,7 @@
+export default function DangerZoneSettings() {
+    return (
+        <div>
+            <h1>DangerZone</h1>
+        </div>
+    )
+}
--- components/settings/NotificationSettings.tsx
+++ components/settings/NotificationSettings.tsx
@@ -42,8 +42,8 @@ const settings: Setting[] = [
 
 export default function NotificationSettings() {
   return (
-    <div className="border border-(--border) rounded-(--radius) bg-(--bg1 overflow-hidden">
-      <CardHeader title="Notifications" dotColor="var-(--amber)" />
+    <div className="bg-(--bg1) rounded-(--radius) border border-(--border) overflow-hidden">
+      <CardHeader title="Notifications" dotColor="var(--amber)" />
     </div>
   );
 }
