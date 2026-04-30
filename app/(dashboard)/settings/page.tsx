b1c16fa556ccf366a89e6ca28d771e94afb50504
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-04-30 20:01:21 +0100

    style: add styling to DangerZoneSettings

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -4,10 +4,14 @@ import DangerZoneSettings from "@/components/settings/DangerZoneSettings";
 
 export default function SettingsPage() {
   return (
-    <div>
-      <ProfileSettings />
-      <NotificationSettings />
-      <DangerZoneSettings />
+    <div className="grid grid-cols-2 item-start gap-2.5">
+      <div className="flex flex-col gap-2.5">
+        <ProfileSettings />
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <NotificationSettings />
+        <DangerZoneSettings />
+      </div>
     </div>
   );
 }
