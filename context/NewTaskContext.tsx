38520ddba2825602bd12943c12aa4c0f93282a68
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-12 23:32:11 +0100

    feat: build task provider and context

--- context/NewTaskContext.tsx
+++ context/NewTaskContext.tsx
@@ -2,8 +2,25 @@
 
 import { createContext, useContext, useState } from "react";
 
-type contextType = {
-    isOpen: boolean;
-    open: () => void;
-    close: () => void;
+type ContextType = {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
+};
+const NewTaskContext = createContext<ContextType | null>(null);
+
+export function NewTaskProvider({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <NewTaskContext.Provider
+      value={{
+        isOpen,
+        open: () => setIsOpen(true),
+        close: () => setIsOpen(false),
+      }}
+    >
+      {children}
+    </NewTaskContext.Provider>
+  );
 }
