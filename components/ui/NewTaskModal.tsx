be45b2766b8cea2032514eccecc68b329d6feb87
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-12 23:38:25 +0100

    chore: create NewTaskModal file

---
+++ components/ui/NewTaskModal.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { useNewTask } from "@/context/NewTaskContext";
+import { X, Plus } from "lucide-react";
+
+export default function NewTaskModal() {
+    return (
+        <>
+        {/* Backdrop */}
+        {/* Modal */}
+        </>
+    )
+}
