ebb21dcf91e4f54e4d7fa8a7bfca68b07e9c14d3
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-13 10:23:05 +0100

    feat: build new task modal ui

--- components/ui/NewTaskModal.tsx
+++ components/ui/NewTaskModal.tsx
@@ -5,10 +5,34 @@ import { useNewTask } from "@/context/NewTaskContext";
 import { X, Plus } from "lucide-react";
 
 export default function NewTaskModal() {
-    return (
-        <>
-        {/* Backdrop */}
+  return (
+    <>
+      {/* Backdrop */}
+      <div className="fixed inset-0" onClick={close}>
         {/* Modal */}
-        </>
-    )
+        <div className="fixed">
+          {/* Header */}
+          <div>
+            <span>New Task</span>
+            <button>
+              <X size={14} />
+            </button>
+          </div>
+          {/* Body */}
+          <div>
+            {/* Title */}
+            <input type="text" />
+          </div>
+        </div>
+        {/* Footer */}
+        <div>
+          <button>Cancel</button>
+          <button>
+            <Plus size={14} />
+            Create Task
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
