b12a099d0a6d7727a95e2624c0c7bcd59b6d6cbb
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-13 10:26:14 +0100

    fix: add state update for task content

--- context/NewTaskContext.tsx
+++ context/NewTaskContext.tsx
@@ -7,22 +7,27 @@ type ContextType = {
   open: () => void;
   close: () => void;
 };
+
 const NewTaskContext = createContext<ContextType | null>(null);
 
 export function NewTaskProvider({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <NewTaskContext.Provider value={{ isOpen, open, close }}>
+    <NewTaskContext.Provider
+      value={{
+        isOpen,
+        open: () => setIsOpen(true),
+        close: () => setIsOpen(false),
+      }}
+    >
       {children}
     </NewTaskContext.Provider>
   );
 }
 
 export function useNewTask() {
-    const ctx = useContext(NewTaskContext);
-    if(!ctx) {
-        throw new Error("useNewTask must be used inside NewTaskProvider");
-        return ctx;
-    }
+  const ctx = useContext(NewTaskContext);
+  if (!ctx) throw new Error("useNewTask must be used inside NewTaskProvider");
+  return ctx;
 }
