bab67bd5b3bae4be08989e4682f4d90453fd11a7
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-13 13:35:23 +0100

    Feat: define sidebar context type

--- context/SidebarContext.tsx
+++ context/SidebarContext.tsx
@@ -1,3 +1,10 @@
 "use client";
 
 import { createContext, useContext, useState } from "react";
+
+type ContextType = {
+  collapsed: boolean;
+  toggle: () => void;
+};
+
+const SidebarContext = createContext<ContextType | null>(null);
