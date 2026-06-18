8f09839cf24ece11f6cdf2bea574fb7958fe09e1
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-18 17:26:32 +0100

    chore: remove errors

--- components/layout/sidebar/Sidebar.tsx
+++ components/layout/sidebar/Sidebar.tsx
@@ -2,22 +2,22 @@
 import SidebarLogo from "./SidebarLogo";
 import SidebarNav from "./SidebarNav";
 import SidebarUser from "./SidebarUser";
-import { useSidebar } from "@/context/SidebarContext";
+// import { useSidebar } from "@/context/SidebarContext";
 
 export default function Sidebar() {
-  const { collapsed } = useSidebar();
+  // const { collapsed } = useSidebar();
 
   return (
     <aside
       className="flex flex-col shrink-0 overflow-hidden border-r border-[var(--border)] bg-[var(--bg1)] transition-all duration-200"
       style={{
-        width: collapsed ? "56px" : "var(--sidebar-width)",
+        width: "56px var(--sidebar-width)",
         height: "100vh",
       }}
     >
-      <SidebarLogo collapsed={collapsed} />
-      <SidebarNav collapsed={collapsed} />
-      <SidebarUser collapsed={collapsed} />
+      <SidebarLogo />
+      <SidebarNav />
+      <SidebarUser />
     </aside>
   );
 }
