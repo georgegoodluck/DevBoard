76ca66dc51629201093715b52bf86f2a8473109e
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-27 16:56:18 +0100

    feat: implement collapsible state in sidebar nav

--- components/layout/sidebar/SidebarNav.tsx
+++ components/layout/sidebar/SidebarNav.tsx
@@ -1,25 +1,31 @@
 import NavLink from "./NavLink";
 import { navItems, accountItems } from "./nav-items";
 
-function SectionLabel({ children }: { children: React.ReactNode }) {
+function SectionLabel({
+  children,
+  collapsed,
+}: {
+  children: React.ReactNode;
+  collapsed: boolean;
+}) {
+  if (collapsed) return <div className="h-[20px]" />;
   return (
-    <div className="pt-4 pb-1 pl-3.5 font-mono text-[10px] font-medium tracking-[0.12em] uppercase text-(--text3)">
+    <div className="pt-4 pb-1 pl-[14px] font-mono text-[10px] font-medium tracking-[0.12em] uppercase text-[var(--text3)]">
       {children}
     </div>
   );
 }
 
-export default function SidebarNav() {
+export default function SidebarNav({ collapsed }: { collapsed: boolean }) {
   return (
     <nav className="flex-1 py-2">
-      <SectionLabel>Workspace</SectionLabel>
+      <SectionLabel collapsed={collapsed}>Workspace</SectionLabel>
       {navItems.map((item) => (
-        <NavLink key={item.href} item={item} />
+        <NavLink key={item.href} item={item} collapsed={collapsed} />
       ))}
-
-      <SectionLabel>Account</SectionLabel>
+      <SectionLabel collapsed={collapsed}>Account</SectionLabel>
       {accountItems.map((item) => (
-        <NavLink key={item.href} item={item} />
+        <NavLink key={item.href} item={item} collapsed={collapsed} />
       ))}
     </nav>
   );
