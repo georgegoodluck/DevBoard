e67c3f0f59473cf34e90b4b92093280d7fa4bdb2
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-27 20:41:27 +0100

    feat: wire StatCardGrid into overview

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,4 +1,4 @@
-import StatGrid from "@/components/overview/StatGrid";
+import StatCardGrid from "@/components/overview/StatCardGrid";
 import ActiveProjects from "@/components/overview/ActiveProjects";
 import RecentTasks from "@/components/overview/RecentTasks";
 import SprintVelocity from "@/components/overview/SprintVelocity";
@@ -8,7 +8,8 @@ import Deadlines from "@/components/overview/Deadlines";
 export default function OverviewPage() {
   return (
     <div className="flex flex-col gap-2">
-      <StatGrid />
+      <StatCardGrid />
+
       {/* Two Column Layout */}
       <div className="flex gap-2.5">
         {/* Left Column */}
