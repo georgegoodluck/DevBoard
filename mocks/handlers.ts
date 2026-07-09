d49bc765c4c01a086aabcf8803e1f2dce1125b28
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-09 21:30:15 +0100

    feat: add export handlers

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,8 +1,16 @@
 import { http, HttpResponse } from "msw";
 import { mockProjects } from "@/mocks/data/projects";
-import { mockTasks } from "@/mocks/tasks";
-import { mockActivity } from "@/mocks/activity";
+import { mockTasks } from "@/mocks/data/tasks";
+import { mockActivity } from "@/mocks/data/activity";
 
 export const handlers = [
-    
-]
+  http.get("api/projects", () => {
+    return HttpResponse.json(mockProjects);
+  }),
+  http.get("api.tasks", () => {
+    return HttpResponse.json(mockTasks);
+  }),
+  http.get("api/activity", () => {
+    return HttpResponse.json(mockActivity);
+  }),
+];
