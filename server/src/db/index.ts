ac2fe3729bfd0962459a26a268c98bd25083bea5
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-18 04:55:06 +0100

    feat(backend/schema): import functions from drizzle

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { env } from "../env";
-// import * as schema from "./schema";
+import * as schema from "./schema";
 
 // Create the postgres connection
 const client = postgres(env.DATABASE_URL, { max: 10 });
---
+++ server/src/db/schema.ts
@@ -0,0 +1,9 @@
+import {
+  pgTable,
+  text,
+  integer,
+  timestamp,
+  pgEnum,
+  uuid,
+} from "drizzle-orm/pg-core";
+
