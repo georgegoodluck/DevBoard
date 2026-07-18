b01e309529261bc859f95f5af2bed20e44fbcf4b
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-18 05:03:41 +0100

    feat(backend/schema): create taskStatus, taskPriority and activityType enums

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -7,11 +7,27 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
-// Enums
-export const projectStatusEnum = pgEnum("project_statis", [
+// Enums - restricts values to a predefined set that's enforced at the database level
+export const projectStatusEnum = pgEnum("project_status", [
   "Planning",
   "In Progress",
   "Review",
   "Active",
   "Archived",
+]);
+
+export const taskStatusEnum = pgEnum("task_status", [
+  "Todo",
+  "In Progress",
+  "Done",
+]);
+
+export const taskPriorityEnum = pgEnum("task_priority", ["high", "mid", "low"]);
+export const activityTypeEnum = pgEnum("activity_type", [
+  "merge",
+  "task",
+  "comment",
+  "deploy",
+  "ci",
+  "update",
 ]);
