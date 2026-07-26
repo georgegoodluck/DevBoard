a11dc9931c783b777bba1d028edec667285c19f5
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-26 20:22:37 +0100

    feat(plugins): write CORS configuration options

--- server/src/plugins/cors.ts
+++ server/src/plugins/cors.ts
@@ -1,2 +1,10 @@
 import { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
+
+export async function corsPlugin(app: FastifyInstance) {
+  await app.register(cors, {
+    origin: ["https://localhost:3000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  });
+}
