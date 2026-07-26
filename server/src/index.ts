d20f71e12e04efe7b075575abec69dcb879d5044
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-26 22:59:46 +0100

    feat: write plugin and route registration code

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,5 +5,13 @@ import { projectRoutes } from "./routes/projects";
 import { taskRoutes } from "./routes/tasks";
 import { activityRoutes } from "./routes/activity";
 
+const app = Fastify({ logger: true });
+async function main() {
+  // Register plugins
+  await corsPlugin(app); // Configures CORS middleware so frontend clients can communicate with the API
 
-
+  // Register routes
+  await app.register(projectRoutes);
+  await app.register(taskRoutes);
+  await app.register(activityRoutes);
+}
