4f7bf83832610e6fe883ac4f660fa827e3a78f6a
George Goodluck <georgegoodluck.dev@gmail.com>
2026-07-28 22:40:04 +0100

    feat(app/login): write login page template

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/lib/supabase/client";
+import Image from "next/image";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const supabase = createClient();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+  return (
+    <>
+      <h1>Login Page</h1>
+    </>
+  );
+}
