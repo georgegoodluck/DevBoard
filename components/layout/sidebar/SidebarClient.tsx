33504b4bad6c5ac886afebb2c24b6745a991a847
George Goodluck <georgegoodluck.dev@gmail.com>
2026-08-07 06:43:05 +0100

    feat: define sidebar client props type for  user info

--- components/layout/sidebar/SidebarClient.tsx
+++ components/layout/sidebar/SidebarClient.tsx
@@ -4,3 +4,7 @@ import { useSidebar } from "@/context/SidebarContext";
 import SidebarLogo from "./SidebarLogo";
 import SidebarNav from "./SidebarNav";
 import SidebarUser from "./SidebarUser";
+
+type Props = {
+  user: { name: string; role: string; initials: string } | null;
+};
